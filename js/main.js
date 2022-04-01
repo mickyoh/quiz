335fddceea36e6912d30b1e51f383991ce64e82d
mitsuki <96933082+mickyoh@users.noreply.github.com>
2022-04-01 16:34:40 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,86 @@
+'use strict';
+
+{
+  const question = document.getElementById('question');
+  const choices = document.getElementById('choices');
+  const btn = document.getElementById('btn');
+  const result = document.getElementById('result');
+  const scoreLabel = document.querySelector('#result > p');
+
+  const quizSet = shuffle([
+    {q: '近所の野良猫の名前は？', c: ['たま', 'もも', 'ゆめ']},
+    {q: 'パチンコで有名なのは？', c: ['げんさん', 'しのさん', 'やまさん']},
+    {q: 'イノッチ一押しアイドルは？', c: ['カルマ', '上原亜衣', 'ひかるげんじ']},
+  ]);
+  let currentNum = 0;
+  let isAnswered;
+  let score = 0;
+
+  function shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[j], arr[i]] = [arr[i], arr[j]];
+    }
+    return arr;
+  }
+
+  function checkAnswer(li) {
+      if (isAnswered) {
+          return;
+      }
+      isAnswered = true;
+
+      if(li.textContent === quizSet[currentNum].c[0]) {
+          li.classList.add('correct');
+          score++;
+
+      } else {
+          li.classList.add('wrong');
+      }
+      btn.classList.remove('disabled');
+    }
+
+
+
+  function setQuiz() {
+    isAnswered = false;
+    question.textContent = quizSet[currentNum].q;
+
+    while(choices.firstChild) {
+      choices.removeChild(choices.firstChild);
+    }
+
+    const shuffledChoices = shuffle([...quizSet[currentNum].c]);
+    shuffledChoices.forEach(choice => {
+      const li = document.createElement('li');
+      li.textContent = choice;
+      li.addEventListener('click', () => {
+          checkAnswer(li);
+      });
+      choices.appendChild(li);
+    });
+
+    if (currentNum === quizSet.length - 1) {
+      btn.textContent = 'Show Score';
+    }
+  }
+
+  setQuiz();
+
+  btn.addEventListener('click', () => {
+    if (btn.classList.contains('disabled')) {
+      return;
+    }
+    btn.classList.add('disabled');
+    
+    if(currentNum === quizSet.length - 1) {
+      console.log(`Score: ${score} / ${quizSet.length}`);
+      scoreLabel.textContent = `Score: ${score} / ${quizSet.length}`;
+      result.classList.remove('hidden');
+    } else {
+      currentNum++;
+      setQuiz();
+    }
+  });
+
+  }
